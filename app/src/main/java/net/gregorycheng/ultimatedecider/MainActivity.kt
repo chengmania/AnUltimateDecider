dac4e63a339bf5ff27d583b1934175c55b7ee128
chengmania <gchengrpt@gmail.com>
2019-05-12 17:15:42 -0400

    Adding Comments so I know what I did, for later

--- app/src/main/java/net/gregorycheng/ultimatedecider/MainActivity.kt
+++ app/src/main/java/net/gregorycheng/ultimatedecider/MainActivity.kt
@@ -16,33 +16,37 @@ class MainActivity : AppCompatActivity() {
         decisionList.clear()
 
 
-
+        //set to the button's call and function
         btnDecide.setOnClickListener {
-
+            //checking to see if the list is empty.  If it is then state so.
             if (decisionList.isNotEmpty()) {
 
-
+                //create a random number based on the quantity of the list
                 val random = Random()
                 val randomDecision = random.nextInt(decisionList.count())
-
+                //display the random choice based on the number in the list
                 txtDecision.text = decisionList[randomDecision]
             } else {
                 txtDecision.text = "You have not entered any options"
             }
 
         }
-
+        //set to the button's call and function
         btnAddChoice.setOnClickListener {
+            //created a variable newChoice, from the user input
             val newChoice = eTChoice.text.toString()
+            //adds the user input choice into the list
             decisionList.add(newChoice)
+            //clears the text in the edittext for a new user input
             eTChoice.text.clear()
-
+            //adds the newly inputted choice into a TextView
             txtOptionsList.text = decisionList.toString()
 
             println(decisionList)
         }
-
+        //Reset button, for clearing the list and starting fresh with new inputs from the user
         btnReset.setOnClickListener {
+
             decisionList.clear()
             txtOptionsList.text = decisionList.toString()
             txtDecision.text =" "
